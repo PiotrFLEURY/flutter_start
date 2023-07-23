dbe25fddd4bedcfe8a468e4c6430db939b6e57b4
PiotrFLEURY <piotr.fleury@gmail.com>
2023-07-23 15:10:35 +0200

    chore(riverpod): fix bloc dir

--- lib/providers.dart
+++ lib/providers.dart
@@ -4,8 +4,8 @@ import 'package:flutter_start/data/repositories/package_repository.dart';
 import 'package:flutter_start/data/sources/remote/pub_api.dart';
 import 'package:flutter_start/domain/use_cases/get_package_details.dart';
 import 'package:flutter_start/domain/use_cases/get_package_list.dart';
-import 'package:flutter_start/presentation/details/blocs/details_cubit.dart';
-import 'package:flutter_start/presentation/details/blocs/states/details_state.dart';
+import 'package:flutter_start/presentation/details/notifiers/details_notifier.dart';
+import 'package:flutter_start/presentation/details/notifiers/states/details_state.dart';
 import 'package:flutter_start/presentation/home/notifiers/notifiers.dart';
 import 'package:flutter_start/presentation/home/notifiers/states/states.dart';
 
---
+++ test/fake_test.dart
@@ -0,0 +1,6 @@
+// ignore: unused_import
+import 'package:flutter_start/main.dart';
+
+void main() {
+  // Fake test made in order to get every dart file in coverage
+}
